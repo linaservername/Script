d58a2987f753b366b08c6581e41d6bc846c3d2a1
Xasan <dsa@gmail.com>
2022-09-04 14:21:15 +0500

    Script

---
+++ script.js
@@ -0,0 +1,29 @@
+var name = prompt('Введите своё имя')
+console.log('Моё имя: ' + name);
+var age = +prompt('Введите свой возраст')
+console.log('Мне: ' + age);
+var example1 = +prompt('8 + 4')
+console.log(example1);
+var example2 = +prompt('8 * 4')
+console.log(example2);
+var example3 = +prompt('8 / 4')
+console.log(example3);
+var example4 = +prompt('8 - 4')
+console.log(example4);
+var example5 = +prompt('10 % 3')
+console.log(example5);
+alert('Зайдите пожалуста в консоль');
+
+console.log('8 + 4 = ' + (8 + 4) + ' Ваш ответ: ' + example1);
+console.log('8 * 4 = ' + (8 * 4) + ' Ваш ответ: ' + example2);
+console.log('8 / 4 = ' + (8 / 4) + ' Ваш ответ: ' + example3);
+console.log('8 - 4 = ' + (8 - 4) + ' Ваш ответ: ' + example4);
+console.log('10 % 3 = ' + (10 % 3) + ' Ваш ответ: ' + example5);
+
+var X = prompt('Введите первое число')
+var Y = prompt('Введите второе число')
+var Z = prompt('Введите третье число')
+
+var res = (X + Y + Z) / 2;
+
+alert( 'Ответ: ' + res);
